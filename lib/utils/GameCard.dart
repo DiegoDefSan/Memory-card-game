ffd5ff89e0ccc3257b8cd197bf2ebb2f2dfc25e7
DiegoDefSan <defsan35@gmail.com>
2023-10-18 11:44:14 -0300

    feat: flipped status added

--- lib/utils/GameCard.dart
+++ lib/utils/GameCard.dart
@@ -3,14 +3,7 @@ export 'GameCard.dart';
 class GameCard {
   final int cod;
   final dynamic value;
+  bool isFlipped = false;
 
   GameCard(this.cod, this.value);
-
-  int getCod() {
-    return cod;
-  }
-
-  dynamic getValue() {
-    return value;
-  }
 }
