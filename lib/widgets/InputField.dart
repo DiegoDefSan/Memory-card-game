99c541a904a9975ef5211a40a1b2941adb15b946
DiegoDefSan <defsan35@gmail.com>
2023-10-17 22:29:17 -0300

    feat: controller added on input field

--- lib/widgets/InputField.dart
+++ lib/widgets/InputField.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 class InputField extends StatefulWidget {
   final String hintText;
+  final TextEditingController textController;
 
   const InputField({
     Key? key,
     required this.hintText,
+    required this.textController,
   }) : super(key: key);
 
   @override
@@ -13,25 +15,17 @@ class InputField extends StatefulWidget {
 }
 
 class _InputFieldState extends State<InputField> {
-  final TextEditingController _textEditingController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextField(
-        controller: _textEditingController,
+        controller: widget.textController,
         decoration: InputDecoration(
           border: const UnderlineInputBorder(),
           hintText: widget.hintText,
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _textEditingController.dispose();
-    super.dispose();
   }
 }
