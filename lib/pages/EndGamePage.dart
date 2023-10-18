45e7ce18f459f54c20f69e643f8c560abbf25094
DiegoDefSan <defsan35@gmail.com>
2023-10-18 15:20:08 -0300

    feat: navigation to home page and board game added

--- lib/pages/EndGamePage.dart
+++ lib/pages/EndGamePage.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:jogo_memoria/pages/BoardGamePage.dart';
+import 'package:jogo_memoria/pages/HomePage.dart';
 import 'package:jogo_memoria/widgets/Button.dart';
 import 'package:jogo_memoria/widgets/Navbar.dart';
 import 'package:jogo_memoria/widgets/texts/Header1.dart';
 import 'package:jogo_memoria/widgets/texts/Header2.dart';
 
 class EndGamePage extends StatefulWidget {
-  const EndGamePage({super.key});
+  int tries;
+  int seconds;
+  String name;
+  bool withImages;
+  bool withNumbers;
+
+  EndGamePage({
+    Key? key,
+    required this.tries,
+    required this.seconds,
+    required this.name,
+    required this.withImages,
+    required this.withNumbers,
+  }) : super(key: key);
 
   @override
   State<EndGamePage> createState() => _EndGamePageState();
@@ -23,29 +38,29 @@ class _EndGamePageState extends State<EndGamePage> {
             Container(
               padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 14),
               decoration: BoxDecoration(
-                color: Color(0xFF03045e),
+                color: const Color(0xFF03045e),
                 borderRadius: BorderRadius.circular(10),
               ),
               child: Column(
                 children: [
                   Container(
                     margin: const EdgeInsets.all(10),
-                    child: const Header1(
-                      text: "Parabéns!",
-                      color: Color(0xFFd0d8ff),
+                    child: Header1(
+                      text: "Parabéns, " + widget.name + "!",
+                      color: const Color(0xFFd0d8ff),
                     ),
                   ),
                   Container(
                     margin: const EdgeInsets.all(10),
-                    child: const Column(
+                    child: Column(
                       children: [
                         Header2(
-                          text: "Tentos: 0",
-                          color: Color(0xFFd0d8ff),
+                          text: "Tentos: ${widget.tries}",
+                          color: const Color(0xFFd0d8ff),
                         ),
                         Header2(
-                          text: "Tempo: 00:00:00",
-                          color: Color(0xFFd0d8ff),
+                          text: "Segundos: ${widget.seconds}",
+                          color: const Color(0xFFd0d8ff),
                         ),
                       ],
                     ),
@@ -56,15 +71,33 @@ class _EndGamePageState extends State<EndGamePage> {
                       children: [
                         Button(
                           text: "Jogar de novo",
-                          onPressed: () {},
-                          backgroundColor: Color(0xFF7b87ff),
-                          textColor: Color(0xFFd0d8ff),
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => BoardGamePage(
+                                  withImages: widget.withImages,
+                                  withNumbers: widget.withNumbers,
+                                  name: widget.name,
+                                ),
+                              ),
+                            );
+                          },
+                          backgroundColor: const Color(0xFF7b87ff),
+                          textColor: const Color(0xFFd0d8ff),
                         ),
                         Button(
                           text: "Voltar para o início",
-                          onPressed: () {},
-                          backgroundColor: Color(0xFFd0d8ff),
-                          textColor: Color(0xFF03045e),
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => HomePage(),
+                              ),
+                            );
+                          },
+                          backgroundColor: const Color(0xFFd0d8ff),
+                          textColor: const Color(0xFF03045e),
                         ),
                       ],
                     ),
